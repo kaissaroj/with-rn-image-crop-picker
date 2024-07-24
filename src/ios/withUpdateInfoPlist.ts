12de358974f0f804628f2210fb0c8e5191633ede
kais <kaissaroj@gmail.com>
2024-07-24 17:09:50 +0545

    Update withUpdateInfoPlist.ts

--- src/ios/withUpdateInfoPlist.ts
+++ src/ios/withUpdateInfoPlist.ts
@@ -1,21 +1,19 @@
 import { ConfigPlugin, withInfoPlist } from '@expo/config-plugins';
 
 export const withUpdateInfoPlist: ConfigPlugin<{
-	photolibText?: string;
-	cameraText?: string;
-	microText?: string;
-}> = (config, { photolibText, cameraText, microText } = {}) => {
-	const photoDescription = photolibText || 'Allow $(PRODUCT_NAME) to access your photo library';
-	const CameraDescription = cameraText || 'Allow $(PRODUCT_NAME) to access your camera';
-	const MicrophoneDescription = microText || 'Allow $(PRODUCT_NAME) to access your microphone';
+    photoLibraryUsageText?: string;
+    cameraText?: string;
+    microText?: string;
+}> = (config, { photoLibraryUsageText, cameraText, microText } = {}) => {
+    const photoLibraryDescription = photoLibraryUsageText || 'Allow $(PRODUCT_NAME) to access selected photos';
+    const cameraDescription = cameraText || 'Allow $(PRODUCT_NAME) to access your camera';
 
-	return withInfoPlist(config, (config) => {
-		config.modResults.NSPhotoLibraryUsageDescription = photoDescription;
-		config.modResults.NSCameraUsageDescription = CameraDescription;
-		config.modResults.NSMicrophoneUsageDescription = MicrophoneDescription;
-
-		return config;
-	});
+    return withInfoPlist(config, (config) => {
+        config.modResults.NSPhotoLibraryUsageDescription = photoLibraryDescription;
+        config.modResults.PHPhotoLibraryPreventAutomaticLimitedAccessAlert = true;
+        config.modResults.NSCameraUsageDescription = cameraDescription;
+        return config;
+    });
 };
 
 export default withUpdateInfoPlist;
