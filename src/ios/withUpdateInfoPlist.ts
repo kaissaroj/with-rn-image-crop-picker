1122287abfefb628b310c5b809cb7281b83bb25b
kais <kaissaroj@gmail.com>
2024-07-25 10:12:28 +0545

    Update withUpdateInfoPlist.ts

--- src/ios/withUpdateInfoPlist.ts
+++ src/ios/withUpdateInfoPlist.ts
@@ -3,13 +3,13 @@ import { ConfigPlugin, withInfoPlist } from '@expo/config-plugins';
 export const withUpdateInfoPlist: ConfigPlugin<{
     photoLibraryUsageText?: string;
     cameraText?: string;
-    microText?: string;
-}> = (config, { photoLibraryUsageText, cameraText, microText } = {}) => {
+}> = (config, { photoLibraryUsageText, cameraText } = {}) => {
     const photoLibraryDescription = photoLibraryUsageText || 'Allow $(PRODUCT_NAME) to access selected photos';
     const cameraDescription = cameraText || 'Allow $(PRODUCT_NAME) to access your camera';
 
     return withInfoPlist(config, (config) => {
         config.modResults.NSPhotoLibraryUsageDescription = photoLibraryDescription;
+        config.modResults.NSPhotoLibraryAddUsageDescription = photoLibraryDescription;
         config.modResults.PHPhotoLibraryPreventAutomaticLimitedAccessAlert = true;
         config.modResults.NSCameraUsageDescription = cameraDescription;
         return config;
